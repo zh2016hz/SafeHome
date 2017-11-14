8393fa15b1b479a0d52aa48d45ab04b400fda97f
zhouhao9@xiaoniu66.com <zhouhao9@xiaoniu66.com>
2017-11-14 20:20:29 +0800

    ss

--- app/src/main/java/com/example/xn069392/safehome/activity/MainActivity.java
+++ app/src/main/java/com/example/xn069392/safehome/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.xn069392.safehome;
+package com.example.xn069392.safehome.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.xn069392.safehome.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/xn069392/safehome/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.xn069392.safehome.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.xn069392.safehome.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
